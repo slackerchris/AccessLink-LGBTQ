c92b1e1993caa5eee0f0faea74007bc80e8b7105
slackerchris <45011188+slackerchris@users.noreply.github.com>
2025-07-23 22:43:36 +0000

    restructor

--- AccessLink/index.js
+++
@@ -1,9 +0,0 @@
-import { registerRootComponent } from 'expo';
-import { enableScreens } from 'react-native-screens';
-import App from './src/App';
-
-// Enable react-native-screens for better performance and accessibility
-enableScreens();
-
-// Register the main component
-registerRootComponent(App);
--- AccessLink/src/screens/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all screens from a central location
-export { HomeScreen } from './HomeScreen';
-export { MapScreen } from './MapScreen';
-export { EventsScreen } from './EventsScreen';
-export { ProfileScreen } from './ProfileScreen';
